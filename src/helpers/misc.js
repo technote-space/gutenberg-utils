32e9da982af64cc5e34c6cf521a10787c02d4ce1
Technote <technote.space@gmail.com>
2020-01-05 05:21:00 +0900

    chore: tweaks

--- src/helpers/misc.js
+++ src/helpers/misc.js
@@ -43,18 +43,18 @@ export const setProperty = ( object, key, value ) => {
 };
 
 /**
- * @returns {boolean}
+ * @returns {boolean} is valid useRef?
  */
 export const isValidUseRef = () => !! wp.element.useRef;
 
 /**
  * @param {object} animate animate
- * @returns {boolean}
+ * @returns {boolean} is valid animation?
  */
 export const isValidAnimation = ( animate ) => !! animate;
 
 /**
  * @param {...{}} args args
- * @returns {string|null}
+ * @returns {string|null} animation usa class
  */
-export const getAnimationUseClass = (...args) => isValidAnimation( args.length ? args[ 0 ] : wp.components.Animate ) ? null : 'no-animation';
+export const getAnimationUseClass = ( ...args ) => isValidAnimation( args.length ? args[ 0 ] : wp.components.Animate ) ? null : 'no-animation';
