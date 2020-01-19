cb19f5453804aa6ad48b8fa3ea1eb5ff1974073d
Technote <technote.space@gmail.com>
2020-01-19 16:25:48 +0900

    fix: code formattings

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Icon from './icon';
 import DropdownButton from './dropdown-button';
 import ColorPalette from './color-palette';
-import FontSizePicker from './font-size-picker'
+import FontSizePicker from './font-size-picker';
 
 export { Icon, DropdownButton, ColorPalette, FontSizePicker };
