d044a3a49529172b93453e9d1a87226e5df57764
Technote <technote.space@gmail.com>
2020-09-23 02:09:46 +0900

    chore: tweaks

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,7 +1,7 @@
 /**
  * @param {object} state state
  * @param {string} name name
- * @returns {number} state
+ * @returns {number|undefined} state
  */
 // eslint-disable-next-line no-magic-numbers
-export const getIndex = (state, name) => name in state.index ? state.index[name] : -1;
+export const getIndex = (state, name) => state.index[name];
